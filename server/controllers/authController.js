78562bd6012e52f95d6575f21b1cbec61460697a
SaranshGolash <129923993+SaranshGolash@users.noreply.github.com>
2026-01-30 12:47:17 +0530

    Internal error fixed for authorization

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -28,8 +28,8 @@ export const register = async (req, res) => {
 
     res.cookie('jwt', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
-      sameSite: 'strict', // Prevent CSRF attacks
+      secure: true, 
+      sameSite: 'none', 
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     });
 
@@ -67,8 +67,8 @@ export const googleLogin = async (req, res) => {
         const token = generateToken(user.id);
          res.cookie('jwt', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV !== 'development',
-            sameSite: 'strict',
+            secure: true,
+            sameSite: 'none',
             maxAge: 30 * 24 * 60 * 60 * 1000,
         });
 
@@ -123,8 +123,8 @@ export const login = async (req, res) => {
       
       res.cookie('jwt', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development',
-        sameSite: 'strict',
+        secure: true, 
+        sameSite: 'none',
         maxAge: 30 * 24 * 60 * 60 * 1000,
       });
 
